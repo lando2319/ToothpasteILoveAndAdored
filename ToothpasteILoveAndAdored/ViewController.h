04281c9edbae11abb89095cad8411672a33b3e0b
Mike Land <mikepland@gmail.com>
2014-10-16 10:50:37 -0500

    Initial Commit

---
+++ ToothpasteILoveAndAdored/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToothpasteILoveAndAdored
+//
+//  Created by MIKE LAND on 10/16/14.
+//  Copyright (c) 2014 MIKE LAND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ToothpasteILoveAndAdored/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToothpasteILoveAndAdored
+//
+//  Created by MIKE LAND on 10/16/14.
+//  Copyright (c) 2014 MIKE LAND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
